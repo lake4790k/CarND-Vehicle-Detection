5318fdd1fd2fb33b7545f6fef9f3a725d21e48a2
lake4790k <lake4790k@users.noreply.github.com>
2017-05-22 20:45:37 +0200

    initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import logging
+import matplotlib.pyplot as plt
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
+logging.debug('Vehicule detection initializing')
+
+if __name__ == '__main__':
+    logging.info("STARTING")
