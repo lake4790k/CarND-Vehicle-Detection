ce688ceb07e91011ff265886e82d61146ad0e911
lake4790k <lake4790k@users.noreply.github.com>
2017-05-25 11:03:32 +0200

    lesson code

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
 import numpy as np
 import logging
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.image as mpimg
+import cv2
+from skimage.feature import hog
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
 logging.debug('Vehicule detection initializing')
 
+
+
+
 if __name__ == '__main__':
     logging.info("STARTING")
+
