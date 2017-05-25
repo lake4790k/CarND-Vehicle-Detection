45b4f24b1139a5463b2849d6974d42f2e9a20efc
lake4790k <lake4790k@users.noreply.github.com>
2017-05-25 14:40:27 +0200

    find cars

--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pickle
 import os.path
+from moviepy.editor import VideoFileClip
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 logging.info('Vehicule detection initializing')
@@ -29,7 +30,8 @@ class Config:
         self.spatial_feat = True  # Spatial features on or off
         self.hist_feat = True  # Histogram features on or off
         self.hog_feat = True  # HOG features on or off
-        self.y_start_stop = [400, 650]  # Min and max in y to search in slide_window()
+        self.y_start = 400
+        self.y_stop = 650
 
 
 def bin_spatial(img, size=(32, 32)):
@@ -66,108 +68,116 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
         return features
 
 
-def img_features(img, cfg: Config):
-    features = []
-
+def convert_color(img, cfg: Config):
     if cfg.color_space == 'HSV':
-        feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     elif cfg.color_space == 'LUV':
-        feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
     elif cfg.color_space == 'HLS':
-        feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     elif cfg.color_space == 'YUV':
-        feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
     elif cfg.color_space == 'YCrCb':
-        feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
     else:
-        feature_image = np.copy(img)
-
-    if cfg.spatial_feat:
-        spatial_features = bin_spatial(feature_image, size=cfg.spatial_size)
-        features.append(spatial_features)
-
-    if cfg.hist_feat:
-        hist_features = color_hist(feature_image, nbins=cfg.hist_bins)
-        features.append(hist_features)
-
-    if cfg.hog_feat:
-        if cfg.hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:, :, channel],
-                                                     cfg.orient, cfg.pix_per_cell, cfg.cell_per_block,
-                                                     vis=False, feature_vec=True))
-        else:
-            hog_features = get_hog_features(feature_image[:, :, cfg.hog_channel], cfg.orient,
-                                            cfg.pix_per_cell, cfg.cell_per_block,
-                                            vis=False, feature_vec=True)
-        features.append(hog_features)
-
-    return np.concatenate(features)
+        return np.copy(img)
 
 
-def imgs_features(files, cfg: Config):
-    features = []
-    for file in files:
-        img = mpimg.imread(file)
-        features1 = img_features(img, cfg)
-        features.append(features1)
+class Train:
 
-    return features
+    def __init__(self, cfg: Config):
+        self.cfg = cfg
 
+    def img_features(self, img):
+        features = []
+        cfg = self.cfg
 
-def prepare_all_features(cfg: Config, sample_size=0):
-    cars = []
-    images = glob.glob('vehicles_smallset/*/*.jpeg', recursive=True)
-    #images = glob.glob('vehicles/*/*.png', recursive=True)
-    for image in images:
-        cars.append(image)
-    notcars = []
-    images = glob.glob('non-vehicles_smallset/*/*.jpeg', recursive=True)
-    #images = glob.glob('non-vehicles/*/*.png', recursive=True)
-    for image in images:
-        notcars.append(image)
+        feature_image = convert_color(img, cfg)
 
-    if sample_size > 0:
-        cars = cars[0:sample_size]
-        notcars = notcars[0:sample_size]
+        if cfg.spatial_feat:
+            spatial_features = bin_spatial(feature_image, size=cfg.spatial_size)
+            features.append(spatial_features)
 
-    logging.info('#vehicules: %d #nonVehicules: %d', len(cars),  len(notcars))
+        if cfg.hist_feat:
+            hist_features = color_hist(feature_image, nbins=cfg.hist_bins)
+            features.append(hist_features)
 
-    t = time.time()
+        if cfg.hog_feat:
+            if cfg.hog_channel == 'ALL':
+                hog_features = []
+                for channel in range(feature_image.shape[2]):
+                    hog_features.extend(get_hog_features(feature_image[:, :, channel],
+                                                         cfg.orient, cfg.pix_per_cell, cfg.cell_per_block,
+                                                         vis=False, feature_vec=True))
+            else:
+                hog_features = get_hog_features(feature_image[:, :, cfg.hog_channel], cfg.orient,
+                                                cfg.pix_per_cell, cfg.cell_per_block,
+                                                vis=False, feature_vec=True)
+            features.append(hog_features)
 
-    car_features = imgs_features(cars, cfg)
-    noncar_features = imgs_features(notcars, cfg)
+        return np.concatenate(features)
 
-    logging.info('%.2f Seconds to prepare features', time.time() - t)
+    def imgs_features(self, files):
+        features = []
+        for file in files:
+            img = mpimg.imread(file)
+            features1 = self.img_features(img)
+            features.append(features1)
 
-    return car_features, noncar_features
+        return features
 
+    def prepare_all_features(self, sample_size=0):
+        cars = []
+        images = glob.glob('vehicles_smallset/*/*.jpeg', recursive=True)
+        #images = glob.glob('vehicles/*/*.png', recursive=True)
+        for image in images:
+            cars.append(image)
+        notcars = []
+        images = glob.glob('non-vehicles_smallset/*/*.jpeg', recursive=True)
+        #images = glob.glob('non-vehicles/*/*.png', recursive=True)
+        for image in images:
+            notcars.append(image)
 
-def train_svc(car_features, notcar_features):
-    X = np.vstack((car_features, notcar_features)).astype(np.float64)
-    X_scaler = StandardScaler().fit(X)
-    scaled_X = X_scaler.transform(X)
+        if sample_size > 0:
+            cars = cars[0:sample_size]
+            notcars = notcars[0:sample_size]
 
-    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+        logging.info('#vehicules: %d #nonVehicules: %d', len(cars),  len(notcars))
 
-    rand_state = np.random.randint(0, 100)
-    X_train, X_test, y_train, y_test = train_test_split(
-        scaled_X, y, test_size=0.2, random_state=rand_state)
+        t = time.time()
 
-    logging.info('Feature vector length: %d', len(X_train[0]))
+        car_features = self.imgs_features(cars)
+        noncar_features = self.imgs_features(notcars)
 
-    svc = LinearSVC()
-    t = time.time()
-    svc.fit(X_train, y_train)
+        logging.info('%.2f Seconds to prepare features', time.time() - t)
 
-    logging.info('%.2f Seconds to train SVC', time.time() - t)
-    logging.info('Test Accuracy of SVC = %.4f', svc.score(X_test, y_test))
+        return car_features, noncar_features
 
-    with open('svc.pickle', 'wb') as f:
-        pickle.dump(svc, f)
+    def train_svc(self):
+        car_features, notcar_features = self.prepare_all_features(sample_size=0)
+        X = np.vstack((car_features, notcar_features)).astype(np.float64)
+        X_scaler = StandardScaler().fit(X)
+        scaled_X = X_scaler.transform(X)
 
-    return svc
+        y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+        rand_state = np.random.randint(0, 100)
+        X_train, X_test, y_train, y_test = train_test_split(
+            scaled_X, y, test_size=0.2, random_state=rand_state)
+
+        logging.info('Feature vector length: %d', len(X_train[0]))
+
+        svc = LinearSVC()
+        t = time.time()
+        svc.fit(X_train, y_train)
+
+        logging.info('%.2f Seconds to train SVC', time.time() - t)
+        logging.info('Test Accuracy of SVC = %.4f', svc.score(X_test, y_test))
+
+        with open('svc.pickle', 'wb') as f:
+            pickle.dump({'svc': svc, 'scaler': X_scaler}, f)
+
+        return svc
 
 
 def load_svc():
@@ -176,15 +186,92 @@ def load_svc():
             return pickle.load(f)
 
 
+class FindCars:
+
+    def __init__(self, cfg: Config, svc, X_scaler, scale):
+        self.cfg = cfg
+        self.svc = svc
+        self.X_scaler = X_scaler
+        self.scale = scale
+
+    def find_cars(self, img):
+        draw_img = np.copy(img)
+        #img = img.astype(np.float32) / 255
+        cfg = self.cfg
+
+        img_tosearch = img[cfg.y_start:cfg.y_stop, :, :]
+        ctrans_tosearch = convert_color(img_tosearch, cfg)
+        if self.scale != 1:
+            imshape = ctrans_tosearch.shape
+            ctrans_tosearch = cv2.resize(ctrans_tosearch,
+                                (np.int(imshape[1] / self.scale), np.int(imshape[0] / self.scale)))
+
+        ch1 = ctrans_tosearch[:, :, 0]
+        ch2 = ctrans_tosearch[:, :, 1]
+        ch3 = ctrans_tosearch[:, :, 2]
+
+        nxblocks = (ch1.shape[1] // cfg.pix_per_cell) - cfg.cell_per_block + 1
+        nyblocks = (ch1.shape[0] // cfg.pix_per_cell) - cfg.cell_per_block + 1
+        nfeat_per_block = cfg.orient * cfg.cell_per_block ** 2
+
+        # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+        window = 64
+        nblocks_per_window = (window // cfg.pix_per_cell) - cfg.cell_per_block + 1
+        cells_per_step = 2  # Instead of overlap, define how many cells to step
+        nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+        nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+        hogs = []
+        if cfg.hog_channel == 'ALL':
+            hogs.append(get_hog_features(ch1, cfg.orient, cfg.pix_per_cell, cfg.cell_per_block, feature_vec=False))
+            hogs.append(get_hog_features(ch2, cfg.orient, cfg.pix_per_cell, cfg.cell_per_block, feature_vec=False))
+            hogs.append(get_hog_features(ch3, cfg.orient, cfg.pix_per_cell, cfg.cell_per_block, feature_vec=False))
+        else:
+            hogs.append(get_hog_features(ctrans_tosearch[:, :, cfg.hog_channel], cfg.orient, cfg.pix_per_cell, cfg.cell_per_block, feature_vec=False))
+
+        for xb in range(nxsteps):
+            for yb in range(nysteps):
+                ypos = yb * cells_per_step
+                xpos = xb * cells_per_step
+
+                if cfg.hog_channel == 'ALL':
+                    hog_feat1 = hogs[0][ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                    hog_feat2 = hogs[0][ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                    hog_feat3 = hogs[0][ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                    hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+                else:
+                    hog_features = hogs[0][ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+
+                xleft = xpos * cfg.pix_per_cell
+                ytop = ypos * cfg.pix_per_cell
+
+                subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+
+                spatial_features = bin_spatial(subimg, size=cfg.spatial_size)
+                hist_features = color_hist(subimg, nbins=cfg.hist_bins)
+
+                features = np.hstack((spatial_features, hist_features, hog_features))
+                features = features.reshape(1, -1)
+                test_features = self.X_scaler.transform(features)
+                is_car = self.svc.predict(test_features)
+
+                if is_car == 1:
+                    xbox_left = np.int(xleft * self.scale)
+                    ytop_draw = np.int(ytop * self.scale)
+                    win_draw = np.int(window * self.scale)
+                    cv2.rectangle(draw_img, (xbox_left, ytop_draw + cfg.y_start),
+                                  (xbox_left + win_draw, ytop_draw + win_draw + cfg.y_start),
+                                  (0, 0, 255), 6)
+
+        return draw_img
 
 
 def main():
 
     cfg = Config()
-    car_features, notcar_features = prepare_all_features(cfg, sample_size=0)
-    svc = train_svc(car_features, notcar_features)
+    train = Train(cfg)
+    svc = train.train_svc()
 
-    # scv = load_svc()
 
 
 if __name__ == '__main__':
